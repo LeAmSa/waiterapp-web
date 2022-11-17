e499c78eabe65fab1f07eef6b43c64f90b7d92d0
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-16 21:27:30 -0300

    Add tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx", "./index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    colors: {
+      red: {
+        light: "#FFABAD",
+        DEFAULT: "#D73035",
+        dark: "#8A1114",
+      },
+      gray: {
+        0: "#FFFFFF",
+        100: "#F2F2F2",
+        200: "#CCCCCC",
+        300: "#999999",
+        400: "#666666",
+        500: "#333333",
+      },
+    },
+  },
+  plugins: [],
+};
